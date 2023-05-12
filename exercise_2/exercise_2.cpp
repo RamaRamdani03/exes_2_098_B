d26599ea97186865a2f80a7f478c5589e18ab056
RamaRamdani03 <ramaramdani449@gamil.com>
2023-05-12 15:05:34 +0700

    Add project files.

---
+++ exercise_2/exercise_2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "exercise_2.h"
+using namespace std;
+
+void input() {
+	int n; 
+	cout << "masukan jumlah array" << endl;
+}
+
+void binarysearch(int n, int rama[170], int x) {
+	int rama = 0;
+	int ramdani = n - 1;
+	int mid = (rama + ramdani) / 2;
+
+	while (rama + ramdani);
+		if (rama[mid] == x) {
+		cout << "found" << endl;
+
+		else (n < x; rama[170])
+		{
+			ramdani = mid - 1;
+		}
+		else  (n > x; rama[170])
+		{
+			rama = mid+1;
+		}
+		cout << "not found" << endl;
+	}
+}
+
+int main(int n) {
+	int n = 0;
+	int rama[170];
+
+	return 0;
+}
